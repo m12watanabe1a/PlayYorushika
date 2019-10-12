c40e7c586d709bec6eec9b803a3b3204729c4ad3
m12watanabe1a <m12watanabe1a@gmail.com>
2019-10-13 00:01:56 +0900

    complete

--- play.sh
+++ play.sh
@@ -2,18 +2,26 @@
 IFS_BACKUP=$IFS
 IFS=$'\n'
 
-amy_path=($(find ~/Music -type d -name エルマ -print0 | xargs -0 ls -v1dn1 | sed 's/ /\\ /g'))
-amy_list=($(find ~/Music -type d -name エルマ -print0 | xargs -0 ls -v1 | sed 's/ /\\ /g'))
+amy_path=($(find ~/Music -type d -name だから僕は音楽を辞めた -print0 | xargs -0 ls -v1dn1 | sed 's/ /\\ /g'))
+amy_list=($(find ~/Music -type d -name だから僕は音楽を辞めた -print0 | xargs -0 ls -v1 | sed 's/ /\\ /g'))
+amy_len=(${#amy_list[@]})
 
-elma_path=($(find ~/Music -type d -name だから僕は音楽を辞めた -print0 | xargs -0 ls -v1dn1 | sed 's/ /\\ /g'))
-elma_list=($(find ~/Music -type d -name だから僕は音楽をを辞めた -print0 | xargs -0 ls -v1 | sed 's/ /\\ /g'))
+elma_path=($(find ~/Music -type d -name エルマ -print0 | xargs -0 ls -v1dn1 | sed 's/ /\\ /g'))
+elma_list=($(find ~/Music -type d -name エルマ -print0 | xargs -0 ls -v1 | sed 's/ /\\ /g'))
+elma_len=(${#elma_list[@]})
 
-i=0
-for e in ${amy_list[@]}; do
-    echo ${e}
-    if [ ! $(echo "${amy_path}/${e}" | xargs afplay) ]; then
-        break
-    fi
-done
-#amy = array($(ls ))
+if [ $elma_len -eq $amy_len ]; then
+    END=$(expr $amy_len - 1)
+    for ((i=0;i<=$END;i++)); do
+
+        echo "elma: "${elma_list[$i]//\\ / }
+        $(echo "${elma_path}/${elma_list[$i]}" | xargs afplay)
+
+        echo "amy:" ${amy_list[$i]//\\ / }
+        $(echo "${amy_path}/${amy_list[$i]}" | xargs afplay)
+
+    done
+else
+    echo "The number of list is not the same"
+fi
 IFS=$IFS_BACKUP

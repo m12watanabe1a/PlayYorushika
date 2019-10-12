0e30b1f800653d9f962efd357221505367e0b7a6
m12watanabe1a <m12watanabe1a@gmail.com>
2019-10-12 23:01:47 +0900

    suspend musi

--- play.sh
+++ play.sh
@@ -5,10 +5,15 @@ IFS=$'\n'
 amy_path=($(find ~/Music -type d -name エルマ -print0 | xargs -0 ls -v1dn1 | sed 's/ /\\ /g'))
 amy_list=($(find ~/Music -type d -name エルマ -print0 | xargs -0 ls -v1 | sed 's/ /\\ /g'))
 
+elma_path=($(find ~/Music -type d -name だから僕は音楽を辞めた -print0 | xargs -0 ls -v1dn1 | sed 's/ /\\ /g'))
+elma_list=($(find ~/Music -type d -name だから僕は音楽をを辞めた -print0 | xargs -0 ls -v1 | sed 's/ /\\ /g'))
+
 i=0
 for e in ${amy_list[@]}; do
-    echo "${amy_path}/${amy_list}" | xargs afplay
-    let i++
+    echo ${e}
+    if [ ! $(echo "${amy_path}/${e}" | xargs afplay) ]; then
+        break
+    fi
 done
 #amy = array($(ls ))
 IFS=$IFS_BACKUP

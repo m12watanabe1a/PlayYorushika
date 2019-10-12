4bdc16b98bfe44a17f3101a1109dcce4d4fae736
m12watanabe1a <m12watanabe1a@gmail.com>
2019-10-13 00:09:11 +0900

    fix play order

--- play.sh
+++ play.sh
@@ -14,11 +14,11 @@ if [ $elma_len -eq $amy_len ]; then
     END=$(expr $amy_len - 1)
     for ((i=0;i<=$END;i++)); do
 
-        echo "elma: "${elma_list[$i]//\\ / }
-        $(echo "${elma_path}/${elma_list[$i]}" | xargs afplay)
-
         echo "amy:" ${amy_list[$i]//\\ / }
         $(echo "${amy_path}/${amy_list[$i]}" | xargs afplay)
+
+        echo "elma: "${elma_list[$i]//\\ / }
+        $(echo "${elma_path}/${elma_list[$i]}" | xargs afplay)
 
     done
 else
